4d9ff2af3934c3169ef792a9114b280058d084fd
Flogo <Flogo@Eumeralla>
2011-05-11 19:36:58 +0200

    benchmark more reusable

--- implementation/src/verifyLMCut.py
+++ implementation/src/verifyLMCut.py
@@ -25,6 +25,7 @@ def myLMCut(problemfile, domainfile):
 def malteLMCut(problemfile, domainfile):
     cmd = " ".join([additivehmaxpath, domainfile, problemfile])
     p = Popen(cmd, shell=True, stdout=PIPE)
+    p.wait()
     return int(p.stdout.readlines()[-1]) 
 
 def compareTask(problemfile, domainfile):
@@ -58,7 +59,7 @@ def listproblems(basedir):
         result.append((problemfile, domainfile))
     return sorted(result)
 
-def benchmark():
+def benchmark(domains=None, problems=None):
     benchmarksdir = '../../../downward/benchmarks/'
     lmcut_suite = [(domainname, listproblems("%s%s/" % (benchmarksdir, domainname))) 
                     for domainname in
@@ -68,44 +69,83 @@ def benchmark():
                      'pipesworld-tankage', 'psr-small', 'rovers', 'tpp', 'trucks-strips',
                      'zenotravel']
                   ]
-    results = {}
-    for domain_id, (domainname, filepaths) in enumerate(lmcut_suite[:1]):
-        results[domainname] = {}
-        for problem_id, (problemfile, domainfile) in enumerate(filepaths[:10]):
-            print "Comparing domain %d/%d problem %d/%d" % (domain_id +1, len(lmcut_suite), 
-                                                            problem_id +1, len(filepaths))
-            my_t, malte_t, my_h, malte_h = compareTask(problemfile, domainfile)
-            results[domainname][problemfile] = (my_t, malte_t, my_h, malte_h)
-
-    for (domainname, filepaths) in lmcut_suite:
-        if domainname not in results:
+    resultsfile = open("results.txt", "w")
+    if domains:
+        n_domains = len(domains)
+    else:
+        n_domains = len(lmcut_suite)
+    for domain_id, (domainname, filepaths) in enumerate(lmcut_suite):
+        if problems:
+            n_problems = len(problems)
+        else:
+            n_problems = len(filepaths)
+        if domains and domain_id not in domains:
             continue
-        aggregatedtimedifference = 0
-        aggregatedheuristicdifference = 0
-        maxheuristicdifference = 0
-        minheuristicdifference = 0
-        aggregatedspeedup = 0
-        for (problemfile, domainfile) in filepaths:
-            if problemfile not in results[domainname]:
+        resultsfile.write("domain: %s\n" % domainname)
+        for problem_id, (problemfile, domainfile) in enumerate(filepaths):
+            if problems and problem_id not in problems:
                 continue
-            (my_t, malte_t, my_h, malte_h) = results[domainname][problemfile]
+            print "Comparing domain %d/%d (%s) problem %d/%d" % (domain_id +1, n_domains,
+                                                                 domainname, 
+                                                                 problem_id +1, n_problems)
+            my_t, malte_t, my_h, malte_h = compareTask(problemfile, domainfile)
+            resultsfile.write("%s %s %s %s %s\n" % (problemfile, my_t, malte_t, my_h, malte_h))
+            resultsfile.flush()
+    resultsfile.close()
+
+class DomainResults:
+    def __init__(self, name, problemresults):
+        self.name = name
+        self.problemresults = problemresults
+        aggregatedtimedifference = 0
+        aggregatedspeedup = 0
+        aggregatedheuristicdifference = 0
+        self.maxheuristicdifference = 0
+        self.minheuristicdifference = 0
+        for (my_t, malte_t, my_h, malte_h) in problemresults.values():
             aggregatedtimedifference += malte_t - my_t
             aggregatedheuristicdifference  += malte_h - my_h
-            maxheuristicdifference = max(maxheuristicdifference, malte_h - my_h)
-            minheuristicdifference = min(minheuristicdifference, malte_h - my_h)
+            self.maxheuristicdifference = max(self.maxheuristicdifference, malte_h - my_h)
+            self.minheuristicdifference = min(self.minheuristicdifference, malte_h - my_h)
             aggregatedspeedup += malte_t / my_t
-        results[domainname]["averagetimedifference"] = aggregatedtimedifference / len(filepaths)
-        results[domainname]["averagespeedup"] = aggregatedspeedup / len(filepaths)
-        results[domainname]["averageheuristicdifference"] = aggregatedheuristicdifference / len(filepaths)
-        results[domainname]["maxheuristicdifference"] = maxheuristicdifference
-        results[domainname]["minheuristicdifference"] = minheuristicdifference
-        print domainname, " (td: %s, su: %s, hd: %s [%s - %s])" % (
-                                               str(results[domainname]["averagetimedifference"] * 1000),
-                                               str(results[domainname]["averagespeedup"]),
-                                               str(results[domainname]["averageheuristicdifference"]),
-                                               str(results[domainname]["minheuristicdifference"]),
-                                               str(results[domainname]["maxheuristicdifference"]),
-                                              )
-    
+        self.averagetimedifference = aggregatedtimedifference / len(problemresults)
+        self.averagespeedup = aggregatedspeedup / len(problemresults)
+        self.averageheuristicdifference = aggregatedheuristicdifference / len(problemresults)
         
-benchmark()
+
+
+def parse_results(filename):
+    results = []
+    problemresults = {}
+    domainname = None
+    for line in open(filename):
+        if line.startswith("domain: "):
+            if domainname is not None:
+                results.append(DomainResults(domainname, problemresults))
+            domainname = line[7:]
+            problemresults = {}
+        else:
+            tokens = line.strip().split() 
+            if len(tokens) != 5:
+                print "cannot parse result line", line
+                continue
+            (problemfile, my_t, malte_t, my_h, malte_h) = tokens 
+            problemresults[problemfile] = (float(my_t), float(malte_t), int(my_h), int(malte_h))
+    if domainname is not None:
+        results.append(DomainResults(domainname, problemresults))
+    return results
+
+def print_results(results):
+    for domainresults in results: 
+        print domainresults.name, " (td: %s, su: %s, hd: %s [%s - %s])" % (
+                               str(domainresults.averagetimedifference * 1000),
+                               str(domainresults.averagespeedup),
+                               str(domainresults.averageheuristicdifference),
+                               str(domainresults.minheuristicdifference),
+                               str(domainresults.maxheuristicdifference),
+                              )
+
+
+if __name__ == "__main__":
+    benchmark(domains=range(8,9), problems=range(10))
+    print_results(parse_results("results.txt"))
