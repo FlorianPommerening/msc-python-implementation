d27bb162f5a6f5b83020b5314ba15eb4939e170e
Flogo <devnull@localhost>
2012-05-29 13:55:44 +0200

    compiled for grid in basel

--- implementation/src/create-jobs-basel.py
+++ implementation/src/create-jobs-basel.py
@@ -1,158 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
-m21a = {
-        "openstacks-strips":[27, 28],
-}
-
-m21b = {
-        "freecell":[45, 50, 51, 52, 53, 56, 58, 59, 60, 64, 65, 68, 70, 71, 72, 75, 76, 77, 79],
-        "logistics98":[24, 27, 28],
-        "mprime":[13],
-        "openstacks-strips":[23, 24, 26, 27, 28, 29],
-        "pipesworld-tankage":[42, 43, 44, 45, 46, 47, 48, 49],
-        "tpp":[25, 26, 27, 28, 29],
-}
-
-m21c = {
-        "freecell":[44, 47, 50, 52, 53, 54, 56, 58, 59, 60, 62, 65, 66, 68, 69, 70, 71, 72, 74, 77, 78, 79],
-        "logistics98":[24, 27, 28],
-        "mprime":[5, 9, 12, 13, 17, 19, 21, 22, 29, 32],
-        "mystery":[12, 13, 21],
-        "openstacks-strips":[23, 24, 25, 26, 27, 28, 29],
-        "pipesworld-notankage":[45, 46, 47, 48, 49],
-        "pipesworld-tankage":[19, 24, 25, 26, 27, 28, 29, 32, 34, 35, 36, 37, 38, 39, 41, 42, 43, 44, 45, 46, 47, 48, 49],
-        "rovers":[34, 36, 37, 38, 39],
-        "tpp":[18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
-        "zenotravel":[17, 18, 19],
-}
-
-m21d = {
-        "airport":[49],
-        "depot":[21],
-        "freecell":[26, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
-        "logistics98":[17, 18, 20, 21, 24, 26, 27, 28, 29],
-        "mprime":[5, 9, 12, 13, 14, 17, 19, 21, 22, 23, 29],
-        "mystery":[12, 13, 21],
-        "openstacks-strips":[23, 24, 25, 26, 27, 28, 29],
-        "pipesworld-notankage":[46, 48, 49],
-        "pipesworld-tankage":[19, 24, 25, 26, 27, 28, 29, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
-        "rovers":[33, 34, 35, 36, 37, 38, 39],
-        "tpp":[19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
-        "zenotravel":[16, 17, 18, 19],
-}
-
-m21e = {
-        "airport":[49],
-        "depot":[21],
-        "driverlog":[19],
-        "freecell":[20, 24, 26, 27, 28, 30, 32, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
-        "logistics98":[12, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 29],
-        "mprime":[5, 9, 12, 13, 14, 17, 19, 21, 22, 23, 29],
-        "mystery":[12, 13, 21],
-        "openstacks-strips":[23, 24, 26, 27, 28, 29],
-        "pipesworld-notankage":[46, 48, 49],
-        "pipesworld-tankage":[19, 24, 25, 26, 27, 28, 29, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
-        "rovers":[32, 33, 34, 35, 36, 37, 38, 39],
-        "tpp":[19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
-        "zenotravel":[16, 17, 18, 19],
-}
-
-m22a = {
-        "airport":[49],
-        "depot":[21],
-        "driverlog":[15, 16, 17, 18, 19],
-        "freecell":[20, 21, 22, 23, 24, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
-        "logistics98":[12, 17, 18, 19, 21, 24, 27, 28, 29],
-        "mprime":[13],
-        "openstacks-strips":[23, 24, 26, 27, 28, 29],
-        "pipesworld-tankage":[25, 28, 29, 34, 35, 38, 39, 41, 42, 43, 44, 45, 46, 47, 48, 49],
-        "rovers":[36, 37, 38, 39],
-        "tpp":[24, 25, 26, 27, 28, 29],
-        "zenotravel":[18, 19],
-}
-
-m22b = {
-        "airport":[49],
-        "depot":[21],
-        "driverlog":[16, 17, 18, 19],
-        "freecell":[20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
-        "logistics98":[17, 18, 19, 20, 21, 24, 26, 27, 28, 29],
-        "mprime":[5, 9, 12, 13, 17, 19, 21, 22, 23, 29],
-        "mystery":[5, 12, 13, 21],
-        "openstacks-strips":[23, 24, 25, 26, 27, 28, 29],
-        "pipesworld-notankage":[41, 42, 43, 44, 45, 46, 47, 48, 49],
-        "pipesworld-tankage":[18, 19, 22, 24, 25, 26, 27, 28, 29, 32, 33, 34, 35, 36, 37, 38, 39, 41, 42, 43, 44, 45, 46, 47, 48, 49],
-        "rovers":[29, 31, 32, 33, 34, 35, 36, 37, 38, 39],
-        "tpp":[20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
-        "zenotravel":[15, 16, 17, 18, 19],
-}
-
-m22c = {
-        "airport":[49],
-        "depot":[21],
-        "driverlog":[16, 17, 18, 19],
-        "freecell":[20, 21, 22, 23, 24, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
-        "logistics98":[12, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 29],
-        "mprime":[5, 9, 12, 13, 17, 19, 21, 22, 23, 29],
-        "mystery":[12, 21],
-        "openstacks-strips":[17, 23, 24, 25, 26, 27, 28, 29],
-        "pipesworld-notankage":[41, 42, 43, 44, 45, 46, 47, 48, 49],
-        "pipesworld-tankage":[18, 19, 22, 23, 24, 25, 26, 27, 28, 29, 32, 33, 34, 35, 36, 37, 38, 39, 41, 42, 43, 44, 45, 46, 47, 48, 49],
-        "rovers":[29, 31, 32, 33, 34, 35, 36, 37, 38, 39],
-        "tpp":[20, 22, 23, 24, 25, 26, 27, 28, 29],
-        "zenotravel":[15, 16, 17, 18, 19],
-}
-
-m22d = {
-        "airport":[49],
-        "depot":[21],
-        "driverlog":[16, 17, 18, 19],
-        "freecell":[20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
-        "logistics98":[17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 29],
-        "mprime":[5, 9, 12, 13, 17, 19, 21, 22, 29],
-        "mystery":[12, 21],
-        "openstacks-strips":[23, 24, 25, 26, 27, 28, 29],
-        "pipesworld-notankage":[41, 42, 43, 44, 45, 46, 47, 48, 49],
-        "pipesworld-tankage":[15, 18, 19, 22, 24, 25, 26, 27, 28, 29, 32, 33, 34, 35, 36, 37, 38, 39, 41, 42, 43, 44, 45, 46, 47, 48, 49],
-        "rovers":[29, 31, 32, 33, 34, 35, 36, 37, 38, 39],
-        "tpp":[20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
-        "zenotravel":[15, 16, 17, 18, 19],
-}
-
-m22e = {
-        "airport":[49],
-        "depot":[21],
-        "driverlog":[16, 17, 18, 19],
-        "freecell":[20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
-        "logistics98":[12, 17, 18, 19, 20, 21, 24, 26, 27, 28, 29],
-        "mprime":[5, 9, 12, 13, 17, 19, 21, 22, 23, 29],
-        "mystery":[12, 21],
-        "openstacks-strips":[23, 24, 25, 26, 27, 28, 29],
-        "pipesworld-notankage":[41, 42, 43, 44, 45, 46, 47, 48, 49],
-        "pipesworld-tankage":[18, 19, 22, 24, 25, 26, 27, 28, 29, 32, 33, 34, 35, 36, 37, 38, 39, 41, 42, 43, 44, 45, 46, 47, 48, 49],
-        "rovers":[29, 31, 32, 33, 34, 35, 36, 37, 38, 39],
-        "tpp":[20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
-        "zenotravel":[15, 16, 17, 18, 19],
-}
-
-
-from collections import defaultdict
-allmissingdd = defaultdict(set)
-for miss in (m21a, m21b, m21c, m21d, m21e, m22a, m22b, m22c, m22d, m22e):
-  for d, probs in miss.items():
-    for p in probs:
-      allmissingdd[d].add(p)
-allmissing = {}
-for d, probs in allmissingdd.items():
-  allmissing[d] = []
-  for p in probs:
-    allmissing[d].append(p)
-
-
-
-from benchmark.problem_suites import problem_subset, LMCUT_EASY, LMCUT_MEDIUM, LMCUT_HARD, LMCUT_SUITE, ZERO_COST_SUITE
+from benchmark.problem_suites import *
 from Cheetah.Template import Template
 import os
 
@@ -164,33 +13,36 @@ queue = "all.q"
 # defines the timeout for one taks. The time format is
 # "hours:minutes:seconds", eg, a value of "0:30:0" sets the timeout to
 # 30 minutes. If timout is set to None, then there is no timeout.
-timeout = '50:00:0'
+timeout = '00:05:30'
 
 # defines the maximum amount of available memory for one task. The
 # value's format is either "<mem>M" or "<mem>G", where <mem> is an
 # integer number, M stands for MByte and G for GByte. If memout is
 # None, then there is no memory bound.
-memout = "3G"
+memout = "2G"
 
 # defines the different configurations of the program to benchmark.
 # The key of each entry represents the name of this configuration, the
 # value gives the name of the executable (here mcta), and the
 # arguments.
 configurations = {
-        './logs' : '../hplusbnb ',
+        './logs' : '../../hplusbnb ',
         }
 
 # defines the benchmark instances. Each entry consists of a name for
 # this benchmark (eg. 'C1'), and a string containing the input file(s)
 # for this benchmark.
 benchmarks = {}
-for (domainname, paths) in problem_subset(problems=m22e):
-    print ("'%s':[" % domainname),
+c = 0
+for (domainname, paths) in problem_subset(problem_suite=ACTION_COST_SUITE_ALL):
+    print "%s: %d" % (domainname, len(paths))
+    c += len(paths)
     for i, (p, d) in enumerate(paths):
         problemname = os.path.splitext(os.path.basename(p))[0]
-        print ("'%s'," % problemname),
-        benchmarks["%s_%s" % (domainname, problemname)] = "%s %s" % (p, d)
-    print "],"
+        p_path_on_grid = p.replace('/home/pommerening/projects/fastdownward/benchmarks/', '/home/ifi/pommeren/benchmarks/')
+        d_path_on_grid = d.replace('/home/pommerening/projects/fastdownward/benchmarks/', '/home/ifi/pommeren/benchmarks/')
+        benchmarks["%s_%s" % (domainname, problemname)] = "%s %s" % (p_path_on_grid, d_path_on_grid)
+print c
 
 
 # the create_tasks functions generates a file containing all possible
@@ -209,7 +61,7 @@ for (domainname, paths) in problem_subset(problems=m22e):
 # grid engine. The $JOB_ID is the same for all tasks of the jobfile,
 # the $SGE_TASK_ID is the id for the current task.
 def create_tasks(filename, configurations, benchmarks):
-    tasks = []
+    tasks = ['mkdir logs']
     for conf, cmdline in sorted(configurations.iteritems()):
         for experiment, input_files in sorted(benchmarks.iteritems()):
             tasks.append("%s %s &> %s/%s_$JOB_ID.$SGE_TASK_ID.log" % (cmdline, input_files, conf, experiment))
